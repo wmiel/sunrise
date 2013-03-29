7a8de9f5dd724196a97d5e65cf2b0f45657536d0
Wojtek <wojtek.mielczarek@gmail.com>
2013-03-29 19:07:19 +0100

    Conversion to Julian Day

--- spec/SunriseSpec.js
+++ spec/SunriseSpec.js
@@ -1,7 +1,35 @@
-describe("Sunrise library", function() {
-    var sunrise = Sunrise();
+describe("Sunrise library", function () {
+    var sunriser = sunrise();
 
-    it("should be defined", function(){
-        expect(sunrise).toBeDefined();
+    it("should be defined", function () {
+        expect(sunriser).toBeDefined();
+    });
+
+    describe("Julian day conversion", function () {
+        it("should return correct number of days since January 1, 4713 BCE for January 1, 2000", function(){
+
+            var calculated = sunriser.toJD(new Date(Date.UTC(2000, 0, 1)));
+            expect(calculated).toEqual(2451544.5);
+        });
+        it("should return correct number of days since January 1, 4713 BCE for January 1, 1970", function(){
+
+            var calculated = sunriser.toJD(new Date(Date.UTC(1970, 0, 1)));
+            expect(calculated).toEqual(2440587.5);
+        });
+        it("should return correct number of days since January 1, 4713 BCE for March 18, 2020", function(){
+
+            var calculated = sunriser.toJD(new Date(Date.UTC(2020, 2, 18)));
+            expect(calculated).toEqual(2458926.5);
+        });
+        it("should return correct number of days since January 1, 4713 BCE for November 22, 2089", function(){
+
+            var calculated = sunriser.toJD(new Date(Date.UTC(2089, 10, 22)));
+            expect(calculated).toEqual(2484377.5);
+        });
+        it("should return correct number of days since January 1, 4713 BCE for February 29, 2004", function(){
+
+            var calculated = sunriser.toJD(new Date(Date.UTC(2004, 1, 29)));
+            expect(calculated).toEqual(2453064.5);
+        });
     });
 });
--- src/Sunrise.js
+++ src/Sunrise.js
@@ -1,6 +1,49 @@
-function Sunrise() {
+/*  Calculations based on http://www.navipedia.pl/
+    and "Astronomiczne oblicznenia nie tylko dla geografów" by by Kazimierz Borkowski
+    Uniwersytet Mikołaja Kopernika w Toruniu, Toruń 1991.
+
+ http://www.cs.utsa.edu/~cs1063/projects/Spring2011/Project1/jdn-explanation.html
+ http://quasar.as.utexas.edu/BillInfo/JulianDatesG.html
+
+    alpha - Right ascension (rektasencja)
+    sigma - Declination (deklinacja)
+    phi - latitude (szerokość geograficzna)
+
+    Dates in js since epoch January 1 1970.
+    January 1, 2000 = 2451545
+    Julian dates calculated since noon on January 1, 4713 BCE.
+
+ */
+
+function sunrise() {
     var that = {};
 
+    that.toJD = function (date) {
+        var Y = date.getFullYear(),
+            M = date.getMonth() + 1,
+            D = date.getDate(),
+            A,
+            B,
+            C,
+            E,
+            F;
+
+        if (M <= 2) {
+            Y -= 1;
+            M += 12;
+        }
+
+        //Calc. leap years
+        A = Math.floor(Y / 100);
+        B = Math.floor(A / 4);
+        C = 2 - A + B;//valid only for dates after 1.10.1582
+        E = Math.floor(365.25 * (Y + 4716));
+        F = Math.floor(30.6001 * (M + 1));
+
+        console.log(date);
+        console.log(C + D + E + F - 1524.5);
+        return C + D + E + F - 1524.5;
+    };
 
     return that;
 }
